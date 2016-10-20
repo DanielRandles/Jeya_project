10a09a53bc375d81470d29efb2c3e4b1b3dc87f1
Daniel Randles <danielrandles@gmail.com>
2016-10-20 17:09:03 -0400

    new correlations

--- preliminary_analysis.R
+++ preliminary_analysis.R
@@ -6,10 +6,18 @@ library(ggplot2)
 
 ## read wide version of merged dataset with all variables 
 base_df <- readRDS("base_df.rds")
+
 base_df$Annual_pop <- as.numeric(base_df$Annual_pop)
 
+## transpose immigration_type data from wide to long
+base_df$immigration_type <- gsub('Inflows of asylum seekers by nationality', 'asylum_seekers', base_df$immigration_type)
+base_df$immigration_type <- gsub('Inflows of foreign population by nationality', 'immigrants', base_df$immigration_type)
+base_df <- spread(base_df, immigration_type, No.of.individuals)
+
 ## add summary statistics(mean, SD, correlation) comparing immigration rates by Country for each year
-base_df2 <- base_df %>% group_by(Country) %>% summarise(cor_immigration_year = cor(Year, No.of.individuals))
+for 
+
+base_df2 <- base_df %>% group_by(Country) # %>% summarise(cor_immigration_year = cor(x = Year, y = (immigrants/Annual_pop), use = 'pairwise.complete.obs'))
 
 base_df3 <- base_df %>% group_by(Country, Year) %>% summarise(immigration_mean = mean(No.of.individuals, na.rm=TRUE), 
                                                         SD = sd(No.of.individuals, na.rm=TRUE),
@@ -27,8 +35,5 @@ base_df4 <- base_df3 %>% filter(Country != 'Ireland' & Country != 'Turkey' & Cou
 ggplot(base_df3, aes(Year, immigration_rate )  ) + geom_point() + # + coord_flip() + facet_grid(Country ~.)
   facet_wrap(~Country)                                                                                                          
 
-<<<<<<< HEAD
-## transpose immigration_type data from wide to long
-immigration_type_split <- spread(base_df, immigration_type, No.of.individuals)
-=======
->>>>>>> origin/master
+
+
